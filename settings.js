a07c232fe46c2dbaae226b8e6a4d4d5aa5e562dd
Titus-kipkoech <117470717+Titus-kipkoech@users.noreply.github.com>
2025-04-29 06:15:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,8 @@ const settings = {
   packname: 'Elmax Bot',
   author: '‎',
   botName: "Elmax Bot",
-  botOwner: 'Professor', 254788600830
-  ownerNumber: '919876543210',2
-54788600830//Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Elmax', 
+  ownerNumber: '254788600830',//Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
